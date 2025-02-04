c2d2abac6399e31b545f4fa4a11de50a03f24a7a
Jason Bourgeois <jaserman2004@gmail.com>
2025-02-04 14:21:21 +0100

    executeallcommand[executeallcommand] : add Command executeallcommand

--- EasySave_Project/EasySave_Project/Command/ExecuteAllJobCommand.cs
+++ EasySave_Project/EasySave_Project/Command/ExecuteAllJobCommand.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using EasySave_Project.Manager;
+using EasySave_Project.Model;
+using EasySave_Project.Service;
+using EasySave_Project.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +12,27 @@ namespace EasySave_Project.Command
 {
     public class ExecuteAllJobCommand : ICommand
     {
+        private readonly JobService _jobService = new JobService();
+        private readonly ConsoleView _consoleView = new ConsoleView();
+
         public void Execute()
         {
+            // Retrieve the list of all jobs
+            List<JobModel> jobsList = _jobService.GetAllJobs();
+
+            // Display the list of all jobs
+            _consoleView.ShowJobList(jobsList);
+
+            // Execute all jobs without any user selection
+            foreach (JobModel selectedJob in jobsList)
+            {
+                _jobService.ExecuteOneJob(selectedJob);
+            }
         }
-        
+
         public void GetInstruction()
         {
-            
+            //TODO to be considered when refactoring
         }
     }
 }
