a7942b1c96e710bb2cfeae9d06ffe4918e1d5ea2
ykadri <yanis.0601@hotmail.fr>
2025-02-10 14:52:14 +0100

    feature[add-xml-log] : add xml mod

---
+++ src/EasySave_Library_Log/manager/LogFormatManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave_Library_Log.manager
+{
+    /// <summary>
+    /// Singleton class responsible for managing the log format (JSON or XML).
+    /// </summary>
+    public sealed class LogFormatManager
+    {
+        private static readonly Lazy<LogFormatManager> instance = new(() => new LogFormatManager());
+
+        /// <summary>
+        /// Gets the single instance of LogFormatManager.
+        /// </summary>
+        public static LogFormatManager Instance => instance.Value;
+
+        /// <summary>
+        /// Enumeration to specify log formats.
+        /// </summary>
+        public enum LogFormat
+        {
+            JSON,
+            XML
+        }
+
+        /// <summary>
+        /// The current format of the log entries (JSON or XML).
+        /// </summary>
+        public LogFormat Format { get; private set; } = LogFormat.XML;
+
+        /// <summary>
+        /// Private constructor to prevent instantiation from outside.
+        /// </summary>
+        private LogFormatManager() { }
+
+        /// <summary>
+        /// Sets the log format to the specified value.
+        /// </summary>
+        /// <param name="format">The log format to set.</param>
+        public void SetLogFormat(LogFormat format)
+        {
+            Format = format;
+        }
+    }
+}
