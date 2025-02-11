98124db754e989e659a262a6e98c5b52c080a522
ykadri <yanis.0601@hotmail.fr>
2025-02-11 16:25:24 +0100

    feature[Cryptosoft] : refacto code, remove duplicate code

--- src/EasySave_Project/EasySave_Project/Service/AJobStrategyService.cs
+++ src/EasySave_Project/EasySave_Project/Service/AJobStrategyService.cs
@@ -1,7 +1,9 @@
-﻿using EasySave_Project.Model;
+﻿using EasySave_Library_Log.manager;
+using EasySave_Project.Model;
 using EasySave_Project.Util;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -44,6 +46,65 @@ namespace EasySave_Project.Service
                 ConsoleUtil.PrintTextconsole(TranslationService.GetInstance().GetText("errorReadingFormat") + ex.Message);
                 return false;
             }
+        }
+
+        /// <summary>
+        /// Common method to handle file copying, encryption, and logging.
+        /// </summary>
+        /// <param name="sourceFile">The source file to copy.</param>
+        /// <param name="targetDir">The target directory where the file will be copied.</param>
+        /// <param name="job">The JobModel object representing the job to execute.</param>
+        /// <returns>Elapsed time for encryption or 0 if no encryption occurred.</returns>
+        public long HandleFileOperation(string sourceFile, string targetFile, JobModel job)
+        {
+            TranslationService translator = TranslationService.GetInstance();
+
+            // Copy file to target
+            FileUtil.CopyFile(sourceFile, targetFile, true);
+
+            string formatFile = FileUtil.GetFileExtension(sourceFile);
+            bool shouldEncrypt = IsEncryptedFileFormat(formatFile);
+
+
+            Stopwatch stopwatch = new Stopwatch();
+            double elapsedTime = 0;
+
+            // If encryption option is enabled, encrypt the file after copying
+            if (shouldEncrypt)
+            {
+                try
+                {
+                    stopwatch.Start();
+                    FileUtil.EncryptFile(targetFile, "Cesi2004@+");
+                    string message = $"{translator.GetText("fileCopiedAndEncrypted")}: {sourceFile} -> {targetFile}";
+                    stopwatch.Stop();
+                    elapsedTime = stopwatch.ElapsedMilliseconds;
+
+                    // Log message after encryption
+                    ConsoleUtil.PrintTextconsole(message);
+                    LogManager.Instance.AddMessage(message);
+                }
+                catch (Exception ex)
+                {
+                    ConsoleUtil.PrintTextconsole($"{translator.GetText("errorEncrypting")}: {ex.Message}");
+                    elapsedTime = -1;
+                }
+            }
+            else
+            {
+                string message = $"{translator.GetText("fileCopied")}: {sourceFile} -> {targetFile}";
+                ConsoleUtil.PrintTextconsole(message);
+                LogManager.Instance.AddMessage(message);
+            }
+
+            // Calculate file size and transfer time
+            long fileSize = FileUtil.GetFileSize(sourceFile);
+            double transferTime = FileUtil.CalculateTransferTime(sourceFile, targetFile);
+
+            // Update state in LogManager
+            LogManager.Instance.UpdateState(job.Name, sourceFile, targetFile, fileSize, transferTime, elapsedTime);
+
+            return fileSize;
         }
     }
 }
