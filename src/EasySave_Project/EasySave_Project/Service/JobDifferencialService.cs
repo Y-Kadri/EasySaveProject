547ecc23d6430f7d0b5adec80af11ab5efb72baa
yKadri <yanis.kadri@etu.u-bordeaux.fr>
2025-02-12 15:56:20 +0100

    fix[fix-pourcentage-copy] : directory copy OK

--- src/EasySave_Project/EasySave_Project/Service/JobDifferencialService.cs
+++ src/EasySave_Project/EasySave_Project/Service/JobDifferencialService.cs
@@ -34,11 +34,20 @@ namespace EasySave_Project.Service
                 LogManager.Instance.AddMessage(message);
 
                 job.LastFullBackupPath = null; // Reset full backup path
+
                 new JobCompleteService().Execute(job, backupDir); // Perform a full backup
             }
             else
             {
-               ExecuteDifferentialSave(job, backupDir, job.LastFullBackupPath); // Perform a differential backup
+                // Retrieve total number of files and total size before starting the backup
+                var allFiles = FileUtil.GetFilesRecursively(job.FileSource);
+                int totalFiles = allFiles.Count;
+                long totalSize = allFiles.Sum(FileUtil.GetFileSize);
+
+                int processedFiles = 0;
+                long processedSize = 0;
+
+                ExecuteDifferentialSave(job, job.FileSource, backupDir, job.LastFullBackupPath, totalFiles, totalSize, ref processedFiles, ref processedSize); // Perform a differential backup
             }
 
             job.LastSaveDifferentialPath = backupDir;
@@ -52,7 +61,11 @@ namespace EasySave_Project.Service
         /// <param name="job">The JobModel representing the backup job.</param>
         /// <param name="targetDir">The target directory where the backup will be stored.</param>
         /// <param name="lastFullBackupDir">The last full backup directory used for comparison.</param>
-        private void ExecuteDifferentialSave(JobModel job, string targetDir, string lastFullBackupDir)
+        /// <param name="totalFiles">Total number of files to be processed.</param>
+        /// <param name="totalSize">Total size of the files to be backed up.</param>
+        /// <param name="processedFiles">Reference to the number of processed files.</param>
+        /// <param name="processedSize">Reference to the total size of processed files.</param>
+        private void ExecuteDifferentialSave(JobModel job, string fileSource, string targetDir, string lastFullBackupDir, int totalFiles, long totalSize, ref int processedFiles, ref long processedSize)
         {
             string message;
             TranslationService translator = TranslationService.GetInstance();
@@ -60,24 +73,23 @@ namespace EasySave_Project.Service
             LogManager.Instance.AddMessage(message);
             ConsoleUtil.PrintTextconsole(message);
 
-            // Calculate the total number of files and total size
-            long totalSize = FileUtil.CalculateTotalSize(job.FileSource);
-            int totalFiles = FileUtil.GetFiles(job.FileSource).Count();
-            int processedFiles = 0;
-            long processedSize = 0;
+            bool directoryModified = false; // Flag to check if any file in the directory is modified
 
             // Copy modified files
-            foreach (string sourceFile in FileUtil.GetFiles(job.FileSource))
+            foreach (string sourceFile in FileUtil.GetFiles(fileSource))
             {
-                string fileName = FileUtil.GetFileName(sourceFile);
-                string lastFullBackupFile = FileUtil.CombinePath(lastFullBackupDir, fileName);
-                string targetFile = FileUtil.CombinePath(targetDir, fileName);
+                string relativePath = FileUtil.GetRelativePath(fileSource, sourceFile);
+                string targetFile = FileUtil.CombinePath(targetDir, relativePath);
+                string lastFullBackupFile = FileUtil.CombinePath(lastFullBackupDir, relativePath);
+
+                // Ensure the target directory exists
+                string targetFileDirectory = Path.GetDirectoryName(targetFile);
+                FileUtil.CreateDirectory(targetFileDirectory);
 
                 // Check if the file needs to be copied
                 if (!FileUtil.ExistsFile(lastFullBackupFile) || FileUtil.GetLastWriteTime(sourceFile) > FileUtil.GetLastWriteTime(lastFullBackupFile))
                 {
                     long fileSize = HandleFileOperation(sourceFile, targetFile, job);
-
 
                     processedFiles++;
                     processedSize += fileSize;
@@ -96,25 +108,65 @@ namespace EasySave_Project.Service
                         CurrentSourceFilePath = sourceFile,
                         CurrentDestinationFilePath = targetFile
                     });
+
+                    // Mark that the directory is modified
+                    directoryModified = true;
                 }
             }
 
-            // Recursively copy modified subdirectories
-            foreach (string subDir in FileUtil.GetDirectories(job.FileSource))
+            // Copy modified subdirectories
+            foreach (string subDir in FileUtil.GetDirectories(fileSource))
             {
-                string subDirName = FileUtil.GetDirectoryName(subDir);
-                string lastFullBackupSubDir = FileUtil.CombinePath(lastFullBackupDir, subDirName);
-                string targetSubDir = FileUtil.CombinePath(targetDir, subDirName);
+                string newTargetDir = FileUtil.CombinePath(targetDir, FileUtil.GetFileName(subDir));
+                string newLastFullBackupDir = FileUtil.CombinePath(lastFullBackupDir, FileUtil.GetFileName(subDir));
 
-                FileUtil.CreateDirectory(targetSubDir);
-                ExecuteDifferentialSave(job, targetSubDir, lastFullBackupSubDir);
+
+                if (!FileUtil.ExistsDirectory(newLastFullBackupDir) || FileUtil.GetLastWriteTime(subDir) > FileUtil.GetLastWriteTime(newLastFullBackupDir))
+                {
+                    ExecuteDifferentialSave(job, subDir, newTargetDir, newLastFullBackupDir, totalFiles, totalSize, ref processedFiles, ref processedSize);
+                }
+
+
+
+                /*string relativePath = FileUtil.GetRelativePath(job.FileSource, subDir);
+                string targetSubDir = FileUtil.CombinePath(targetDir, relativePath);
+                string lastFullBackupSubDir = FileUtil.CombinePath(lastFullBackupDir, relativePath);
+
+                // Ensure the subdirectory exists before proceeding
+                if (!FileUtil.ExistsDirectory(lastFullBackupSubDir))
+                {
+                    continue; // Skip if the last full backup directory doesn't exist
+                }
+
+                // Check if any file or subdirectory has been modified in this directory
+                bool subDirModified = false;
+                foreach (string subFile in FileUtil.GetFiles(subDir))
+                {
+                    string lastFullBackupSubFile = FileUtil.CombinePath(lastFullBackupSubDir, FileUtil.GetFileName(subFile));
+
+                    if (!FileUtil.ExistsFile(lastFullBackupSubFile) || FileUtil.GetLastWriteTime(subFile) > FileUtil.GetLastWriteTime(lastFullBackupSubFile))
+                    {
+                        subDirModified = true;
+                        break; // No need to continue checking this subdirectory
+                    }
+                }
+
+                // Proceed with the subdirectory only if modified
+                if (subDirModified)
+                {
+                    FileUtil.CreateDirectory(targetSubDir);
+
+                    // Proceed with the differential save on the subdirectory
+                    ExecuteDifferentialSave(job, targetSubDir, lastFullBackupSubDir, totalFiles, totalSize, ref processedFiles, ref processedSize);
+                }
             }
 
             string endMessage = $"Differential backup {job.Name} completed.";
             ConsoleUtil.PrintTextconsole(endMessage);
-            LogManager.Instance.AddMessage(endMessage);
+            LogManager.Instance.AddMessage(endMessage);*/
+            }
         }
+
     }
 }
 
-          
