01c41d9dab3668d0fdf3647348264178c22eaaa4
Matis <matis.bouquin@etu.u-bordeaux.fr>
2025-02-14 11:19:30 +0100

    feature[front-home] : xml and json

--- src/EasySave_Project/EasySave_Project/Dto/LogDto.cs
+++ src/EasySave_Project/EasySave_Project/Dto/LogDto.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text.Json.Serialization;
+using System.Xml.Serialization;
 using EasySave_Project.Util;
 
 namespace EasySave_Project.Dto
@@ -11,17 +12,41 @@ namespace EasySave_Project.Dto
         public string Name { get; set; }
         public List<LogDataDto> Logs { get; set; }
     }
+    
+    [XmlRoot("Logs")]
+    public class LogsDto
+    {
+        [XmlElement("LogEntry")]
+        public List<LogDataDto> Entries { get; set; }
+    }
 
+    [XmlRoot("LogDataDto")]
     public class LogDataDto
     {
         [JsonConverter(typeof(CustomDateTimeConverterUtil))]
+        [XmlElement("Timestamp")]
         public DateTime Timestamp { get; set; }
+
+        [XmlElement("JobName")]
         public string JobName { get; set; }
+
+        [XmlElement("SourcePath")]
         public string SourcePath { get; set; }
+
+        [XmlElement("TargetPath")]
         public string TargetPath { get; set; }
+
+        [XmlElement("FileSize")]
         public long FileSize { get; set; }
+
+        [XmlElement("TransferTime")]
         public double TransferTime { get; set; }
+
+        [XmlElement("Messages")]
         public List<MessageDto> Messages { get; set; }
+
+        [XmlElement("EncryptionTime")]
+        public double EncryptionTime { get; set; }
     }
 
     public class MessageDto
