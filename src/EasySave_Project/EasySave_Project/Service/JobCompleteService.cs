136b6965a732cd979a33975515623bbc7dee1fdf
Mathias Duprat <mathias.duprat@meta-video.com>
2025-02-20 09:12:16 +0100

    feature[max-size-file-handling] : complete save can know limit 1 large file per 1 and switch to other file

--- src/EasySave_Project/EasySave_Project/Service/JobCompleteService.cs
+++ src/EasySave_Project/EasySave_Project/Service/JobCompleteService.cs
@@ -3,8 +3,10 @@ using EasySave_Library_Log.manager;
 using EasySave_Project.Model;
 using EasySave_Project.Util;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 
 namespace EasySave_Project.Service
 {
@@ -16,7 +18,7 @@ namespace EasySave_Project.Service
     public class JobCompleteService : AJobStrategyService
     {
         public event Action<double> OnProgressChanged;
-        
+
         /// <summary>
         /// Executes the complete backup job for the given JobModel.
         /// </summary>
@@ -37,9 +39,9 @@ namespace EasySave_Project.Service
             int processedFiles = 0;
             long processedSize = 0;
 
-            // Execute full backup process
-            ExecuteCompleteSave(job.FileSource, backupDir, job, totalFiles, totalSize, ref processedFiles, ref processedSize);  
-            
+            // Execute the full backup process
+            ExecuteCompleteSave(job.FileSource, backupDir, job, totalFiles, totalSize, ref processedFiles, ref processedSize);
+
             // Update the last full backup path
             job.LastFullBackupPath = backupDir;
 
@@ -49,45 +51,52 @@ namespace EasySave_Project.Service
         }
 
         /// <summary>
-        /// Implements the logic for performing a complete backup.
-        /// This method copies all files and subdirectories from the source directory
-        /// to the target directory.
+        /// Recursively copies all files and directories from the source directory to the target directory.
         /// </summary>
         /// <param name="sourceDir">The source directory to back up.</param>
-        /// <param name="targetDir">The target directory where the backup will be stored.</param>
-        /// <param name="job">The JobModel representing the backup job.</param>
-        /// <param name="totalFiles">Total number of files to be backed up.</param>
-        /// <param name="totalSize">Total size of all files to be backed up.</param>
-        /// <param name="processedFiles">Reference to the counter for processed files.</param>
-        /// <param name="processedSize">Reference to the counter for processed size.</param>
+        /// <param name="targetDir">The destination directory for the backup.</param>
+        /// <param name="job">The job configuration.</param>
+        /// <param name="totalFiles">Total number of files in the backup.</param>
+        /// <param name="totalSize">Total size of all files in the backup.</param>
+        /// <param name="processedFiles">Counter for processed files (used for progress tracking).</param>
+        /// <param name="processedSize">Counter for processed data size (used for progress tracking).</param>
         private void ExecuteCompleteSave(string sourceDir, string targetDir, JobModel job, int totalFiles, long totalSize, ref int processedFiles, ref long processedSize)
         {
-            var files = FileUtil.GetFiles(sourceDir);
-            TranslationService translator = TranslationService.GetInstance();
+            // Queue to store normal-sized files to be copied first
+            var files = new Queue<string>(FileUtil.GetFiles(sourceDir));
+            // Queue for large files that will be processed later
+            var fallbackQueue = new Queue<string>();
 
-            // Copy all files from the source directory
-            foreach (string sourceFile in files)
-            {
-                string fileName = FileUtil.GetFileName(sourceFile);
-                string targetFile = FileUtil.CombinePath(targetDir, fileName);
-                double progressPourcentage = (double)processedFiles / totalFiles * 100;
-                long fileSize = HandleFileOperation(sourceFile, targetFile, job, progressPourcentage);
-
-                // Update processed file count and total processed size
-                processedFiles++;
-                processedSize += fileSize;
-
-                UpdateBackupState(job, processedFiles, processedSize, totalFiles, totalSize, sourceFile, targetFile, progressPourcentage);
-            }
-            
-
+            // Process the queued files
+            ProcessFilesInQueue(files, fallbackQueue, targetDir, job, ref processedFiles, ref processedSize);
+            // Process large files after regular ones
+            ProcessFallbackQueue(fallbackQueue, targetDir, job, ref processedFiles, ref processedSize);
             // Recursively process subdirectories
+            ProcessSubdirectories(sourceDir, targetDir, job, totalFiles, totalSize, ref processedFiles, ref processedSize);
+        }
+
+        /// <summary>
+        /// Recursively processes subdirectories and performs a full backup for each.
+        /// </summary>
+        /// <param name="sourceDir">The source directory.</param>
+        /// <param name="targetDir">The target backup directory.</param>
+        /// <param name="job">The job configuration.</param>
+        /// <param name="totalFiles">Total number of files in the backup.</param>
+        /// <param name="totalSize">Total size of all files in the backup.</param>
+        /// <param name="processedFiles">Counter for processed files (used for progress tracking).</param>
+        /// <param name="processedSize">Counter for processed data size (used for progress tracking).</param>
+        private void ProcessSubdirectories(string sourceDir, string targetDir, JobModel job, int totalFiles, long totalSize, ref int processedFiles, ref long processedSize)
+        {
             foreach (string subDir in FileUtil.GetDirectories(sourceDir))
             {
-                string fileName = FileUtil.GetFileName(subDir);
-                string targetSubDir = FileUtil.CombinePath(targetDir, fileName);
+                // Get the subdirectory name and construct the target path
+                string subDirName = FileUtil.GetFileName(subDir);
+                string targetSubDir = FileUtil.CombinePath(targetDir, subDirName);
+
+                // Create the subdirectory in the target location
                 FileUtil.CreateDirectory(targetSubDir);
 
+                // Recursively back up the subdirectory
                 ExecuteCompleteSave(subDir, targetSubDir, job, totalFiles, totalSize, ref processedFiles, ref processedSize);
             }
         }
